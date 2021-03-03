f53c6f999973c3bd615d3b400eee75921a8bdf9d
Ahmed Sherif <ahmed.sherif@integrant.com>
2021-03-03 17:56:27 +0200

    add enumutils and lookup controller

---
+++ Orders.API.Utilities/EnumUtils.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+namespace Orders.API.Utilities
+{
+    public class EnumUtils<T>
+    {
+        public static string GetDescription(T enumValue, string defDesc)
+        {
+            var field = enumValue.GetType().GetField(enumValue.ToString());
+
+            var attributes = field?.GetCustomAttributes(typeof(DescriptionAttribute), true);
+            if (attributes != null && attributes.Length > 0)
+            {
+                return ((DescriptionAttribute)attributes[0]).Description;
+            }
+            return defDesc;
+        }
+
+        public static string GetDescription(T enumValue)
+        {
+            return GetDescription(enumValue, string.Empty);
+        }
+
+        public static T FromDescription(string description)
+        {
+            var t = typeof(T);
+            foreach (var fi in t.GetFields())
+            {
+                var attrs = fi.GetCustomAttributes(typeof(DescriptionAttribute), true);
+                if (attrs.Length <= 0) continue;
+                foreach (DescriptionAttribute attr in attrs)
+                {
+                    if (attr.Description.Equals(description))
+                    {
+                        return (T)fi.GetValue(null);
+                    }
+                }
+            }
+            return default(T);
+        }
+
+        public static string GetDisplayName(Enum enumValue)
+        {
+            return enumValue.GetType()
+                            .GetMember(enumValue.ToString())
+                            .First()
+                            .GetCustomAttribute<DisplayAttribute>()
+                            .GetName();
+        }
+
+        public static T GetEnumValue(int intValue)
+        {
+            if (!typeof(T).IsEnum)
+            {
+                throw new Exception("T must be an Enumeration type.");
+            }
+
+            var val = ((T[])Enum.GetValues(typeof(T)))[0];
+
+            foreach (var enumValue in (T[])Enum.GetValues(typeof(T)))
+            {
+                if (!Convert.ToInt32(enumValue).Equals(intValue))
+                {
+                    continue;
+                }
+                val = enumValue;
+                break;
+            }
+            return val;
+        }
+
+        public static List<T> GetAllEnumValueList()
+        {
+            Type enumType = typeof(T);
+
+            if (enumType.BaseType != typeof(Enum))
+                throw new ArgumentException("T must be of type System.Enum");
+
+            Array enumValArray = Enum.GetValues(enumType);
+
+            List<T> enumValList = new List<T>(enumValArray.Length);
+
+            foreach (int val in enumValArray)
+            {
+                enumValList.Add((T)Enum.Parse(enumType, val.ToString()));
+            }
+
+            return enumValList;
+        }
+
+        public static List<T> GetAllEnumValueList(out List<string> lstEnumDescription)
+        {
+            Type enumType = typeof(T);
+            lstEnumDescription = new List<string>();
+            if (enumType.BaseType != typeof(Enum))
+                throw new ArgumentException("T must be of type System.Enum");
+
+            Array enumValArray = Enum.GetValues(enumType);
+
+            List<T> enumValList = new List<T>(enumValArray.Length);
+
+            foreach (int val in enumValArray)
+            {
+                enumValList.Add((T)Enum.Parse(enumType, val.ToString()));
+                lstEnumDescription.Add(GetDescription((T)Enum.Parse(enumType, val.ToString())));
+            }
+            return enumValList;
+        }
+
+        public static List<string> GetAllEnumDescriptionList()
+        {
+            Type enumType = typeof(T);
+            if (enumType.BaseType != typeof(Enum))
+                throw new ArgumentException("T must be of type System.Enum");
+
+            Array enumValArray = Enum.GetValues(enumType);
+
+            List<string> lstEnumDescription = new List<string>();
+
+            foreach (int val in enumValArray)
+            {
+                lstEnumDescription.Add(GetDescription((T)Enum.Parse(enumType, val.ToString())));
+            }
+            return lstEnumDescription;
+        }
+
+        public static List<string> GetAllEnumDescriptionList(List<T> enumsList)
+        {
+            Type enumType = typeof(T);
+            if (enumType.BaseType != typeof(Enum))
+                throw new ArgumentException("T must be of type System.Enum");
+
+            Array enumValArray = enumsList.ToArray();
+
+            List<string> lstEnumDescription = new List<string>();
+
+            foreach (int val in enumValArray)
+            {
+                lstEnumDescription.Add(GetDescription((T)Enum.Parse(enumType, val.ToString())));
+            }
+            return lstEnumDescription;
+        }
+
+        public static string GetDisplayName(T enumValue, string defDesc)
+        {
+            var field = enumValue.GetType().GetField(enumValue.ToString());
+
+            var attributes = field?.GetCustomAttributes(typeof(DisplayAttribute), true);
+            if (attributes != null && attributes.Length > 0)
+            {
+                return ((DisplayAttribute)attributes[0]).Name;
+            }
+            return defDesc;
+        }
+
+        public static string GetDisplayName(T enumValue)
+        {
+            return GetDisplayName(enumValue, string.Empty);
+        }
+
+        public static string GetDisplayDescription(T enumValue, string defDesc)
+        {
+            var field = enumValue.GetType().GetField(enumValue.ToString());
+
+            var attributes = field?.GetCustomAttributes(typeof(DisplayAttribute), true);
+            if (attributes != null && attributes.Length > 0)
+            {
+                return ((DisplayAttribute)attributes[0]).Description;
+            }
+            return defDesc;
+        }
+
+        public static string GetDisplayDescription(T enumValue)
+        {
+            return GetDisplayDescription(enumValue, string.Empty);
+        }
+    }
+}
---
+++ Orders.API/Controllers/LookupController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Orders.API.Core;
+using Orders.API.Utilities;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Orders.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class LookupController : Controller
+    {
+        // GET: /<controller>/caseTypes
+        [HttpGet("caseTypes")]
+        public IActionResult GetCaseTypes()
+        {
+            var caseTypesStrings = EnumUtils<CaseTypes>.GetAllEnumDescriptionList();
+            return Ok(caseTypesStrings);
+        }
+
+        // GET: /<controller>/caseTypes
+        [HttpGet("recordTypes")]
+        public IActionResult GetRecordTypes()
+        {
+            var recordTypesStrings = EnumUtils<RecordTypes>.GetAllEnumDescriptionList();
+            return Ok(recordTypesStrings);
+        }
+    }
+}
